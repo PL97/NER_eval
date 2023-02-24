544833a9c353dbdd432de562f72778c4efab1f4f
PL97 <pengle@126.com>
2023-02-24 12:08:10 -0600

    put script in folder

--- nereval/ner_metrics.py
+++ nereval/ner_metrics.py
@@ -63,30 +63,6 @@ def classifcation_report(tags_true: list, tags_pred: list, mode="lenient") -> di
             print(f"tag({mm}): {ue} \t precision:{metrics[ue][f'{mm}_precision']} \t recall:{metrics[ue][f'{mm}_recall']} \t f1-score:{metrics[ue][f'{mm}_f1-score']}")
     return metrics
         
-            
-    
-
 
 if __name__ == "__main__":
-    
-    
-    test_from_file = False
-    if test_from_file:
-        y_pred, y_true = [], []
-        with open(r"label_test2.txt",'r') as f:
-            for line in f.readlines():
-                line = re.sub("\n", "", line).split("\t")
-                predict.append(line[2])
-                truth.append(line[1])
-        
-        metrics = classifcation_report(y_true, y_pred)
-        import json
-        with open('evaluation.json', 'w') as f:
-            json.dump(metrics, f)
-        # ctrl + shift + I for better display on vscode
-    else:
-        y_true = ['B-PER', 'I-PER', 'O', 'B-ORG', 'B-ORG', 'O', 'O', 'B-PER', 'I-PER', 'O']
-        y_pred = ['O', 'B-PER', 'O', 'B-ORG', 'B-ORG', 'I-ORG', 'O', 'B-PER', 'I-PER', 'O']
-        metrics = classifcation_report(y_true, y_pred)
-    
-    
+    pass
